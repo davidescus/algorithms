0c321ba3d3a9252d1d17908768ef36630366f755
davidescus <rahthman_s@yahoo.com>
2017-10-27 23:25:33 +0300

    Refactoring

--- insertion-sort/insertion-sort.go
+++ insertion-sort/insertion-sort.go
@@ -6,43 +6,54 @@ func main() {
 
 	// sorting in increase order
 	fmt.Println("----------------- Start Ordering Ascending -----------------")
-	var a = [6]int{5, 2, 4, 6, 1, 3}
+	var a = []int{5, 2, 4, 6, 1, 3}
 
-	for j := 1; j < len(a); j++ {
-		val := a[j]
-		fmt.Println("Index j: ", j)
-		fmt.Println("Value: ", val)
-		i := j - 1
-
-		fmt.Println("Index i: ", i)
-		for i > -1 && a[i] > val {
-			fmt.Println("Index i inside second loop: ", i)
-			a[i+1] = a[i]
-			i = i - 1
-			a[i+1] = val
-		}
-		fmt.Println("-- Value of a: ", a, " --\r\n")
-	}
+	var b = a
+	asc := sort(b, "asc")
+	fmt.Println("\n-- Slice after ascending sort: ", asc, " --\r\n")
 
 	fmt.Println("")
 
 	// sorting in increase order
-	fmt.Println("----------------- Start Ordering Descending -----------------")
-	var b = [6]int{5, 2, 4, 6, 1, 3}
+	fmt.Println("\n----------------- Start Ordering Descending -----------------")
+	desc := sort(a, "desc")
+	fmt.Println("\n-- Slice after descending sort: ", desc, " --\r\n")
 
-	for j := 1; j < len(b); j++ {
+}
+
+func sort(a []int, order string) []int {
+
+	fmt.Println("-- Initial value of array: ", a, " --\r\n")
+
+	for j := 1; j < len(a); j++ {
+
+		// store current value of j
 		val := a[j]
-		fmt.Println("Index j: ", j)
-		fmt.Println("Value: ", val)
+
+		// move one index before j
 		i := j - 1
 
-		fmt.Println("Index i: ", i)
-		for i > -1 && b[i] < val {
-			fmt.Println("Index i inside second loop: ", i)
-			b[i+1] = b[i]
-			i = i - 1
-			b[i+1] = val
+		// work only if i is greater than index 0
+		for i > -1 {
+
+			// exit loop based on sorting type
+			if order == "asc" && a[i] < val {
+				break
+			}
+			if order == "desc" && a[i] > val {
+				break
+			}
+
+			// switch right element to left
+			a[i+1] = a[i]
+
+			// switch left element to right
+			a[i] = val
+
+			// decrement i
+			i--
+			fmt.Println("Slice inside second for loop: ", a)
 		}
-		fmt.Println("-- Value of a: ", b, " --\r\n")
 	}
+	return a
 }
